2b363771815161870fb94112f6eea90fcb62a0e9
danya <d4nya1v@gmail.com>
2020-11-22 22:50:51 +0300

    need optimization

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import './App.css';
+import { User } from '../src/components/user'
+import { Page } from '../src/components/page'
+import { getPhotos } from '../src/actions/PageActions'
+import { handleLogin } from '../src/actions/UserActions'
 
 class App extends Component {
-render() {
-  const { user, page } = this.props
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Мой топ фото</h1>
-      </header>
-      <p className="App-intro">Здесь будут мои самые залайканые фото</p>
-      {/* <p>Меня зовут: {this.props.user}</p> */}
-      <p>Привет {user.name} ! </p>
-  <p> У тебя {page.photos.length} фото за {page.year} год</p>
-    </div>
-  );
+  render() {
+    // вытащили handleLoginAction из this.props
+    const { user, page, getPhotosAction, handleLoginAction } = this.props
+    return (
+      <div className="app">
+        <Page
+          photos={page.photos}
+          year={page.year}
+          error={page.error}
+          isFetching={page.isFetching}
+          getPhotos={getPhotosAction}
+        />
+        {/* добавили новые props для User */}
+        <User
+          name={user.name}
+          isFetching={user.isFetching}
+          error={user.error}
+          handleLogin={handleLoginAction}
+        />
+      </div>
+    )
   }
 }
 
-const mapStateToProps = store =>{
-  console.log(store)
-  return{
-    user: store.user,
+const mapStateToProps = store => {
+  return {
+    user: store.user, // вытащили из стора (из редьюсера user все в переменную thid.props.user)
     page: store.page,
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    getPhotosAction: year => dispatch(getPhotos(year)),
+    // "приклеили" в this.props.handleLoginAction функцию, которая умеет диспатчить handleLogin
+    handleLoginAction: () => dispatch(handleLogin()),
+  }
+}
 
-export default connect(mapStateToProps)(App)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
 import App from './App';
 import { Provider } from 'react-redux'
 import { store } from './store/configureStore'
 
 ReactDOM.render(
   <Provider store={store}>
+    <div className='app'>
     <App />
+    </div>
   </Provider>,
   document.getElementById('root')
 );
